2bca76ef4e20a37469a7dba66eed9abd064d78eb
Kavindu Senanayake <gihankavindu789@gmail.com>
2025-03-22 07:52:24 +0530

    Firbase config
    new updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,13 +3,14 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.bloodline"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -41,4 +42,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies{
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
 }
